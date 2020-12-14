912aee1b1d0feee4d5f77e3f809b2707563f46b8
Pablo Tunonbros <tunonbros@gmail.com>
2020-12-14 16:42:09 +0100

    Saving tweet on creation

--- ai-tweet-generate/functions/generate-tweet.py
+++ ai-tweet-generate/functions/generate-tweet.py
@@ -2,14 +2,28 @@ import json
 from libs import gpt
 from libs import handler
 from libs import twitter
+from libs.db import tweets_db
 
 
 def generate_tweet(event):
     body = json.loads(event['body'])
     username = twitter.strip_username(body['username'])
+
+    # Get tweets from twitter
     tweets = twitter.get_tweets(username)
+
+    # Predict with GPT-3
     prediction = gpt.make_prediction(username, tweets)
+
+    # Store on database
+    tweet_id = tweets_db.insert_tweet({
+        'tweet': prediction,
+        'username': username,
+        'views': 0
+    })
+
     return {
+        "tweetId": tweet_id,
         "tweet": prediction,
         "username": username
     }
--- ai-tweet-generate/functions/shared.py
+++ ai-tweet-generate/functions/shared.py
@@ -3,25 +3,12 @@ from libs import handler
 from libs.db import tweets_db
 
 
-def post_shared(event):
-    body = json.loads(event['body'])
-    tweet_id = tweets_db.insert_tweet({
-        'tweet': body['tweet'],
-        'username': body['username'],
-        'views': 0
-    })
-    return {
-        'tweetId': tweet_id
-    }
-
-
-# Don't return everything from database, just these keys
-return_keys = ['tweet', 'username']
-
-
 def get_shared(event):
     tweet_id = event['pathParameters']['tweetId']
     tweet = tweets_db.get_tweet(tweet_id)
+
+    # Don't return everything from database, just these keys
+    return_keys = ['tweet', 'username']
     return handler.serialize(tweet.items(), return_keys)
 
 
@@ -32,14 +19,4 @@ def get(event, context):
         'statusCode': status,
         'headers': handler.get_headers(),
         'body': json.dumps(payload, default=str)
-    }
-
-
-def post(event, context):
-    payload, status = handler.wrap_function(post_shared, event, 201)
-
-    return {
-        'statusCode': status,
-        'headers': handler.get_headers(),
-        'body': json.dumps(payload)
     }
